4a796f0a6dc9b2d50a5af81497afd6e39b0f18e9
sandeepnetwin <sandeep.w@netwinindia.biz>
2015-07-04 17:15:52 +0530

    IP change
    IP change

--- ipchange.php
+++ ipchange.php
@@ -3,18 +3,23 @@
 * @Programmer: AES
 * @Description: When device ip change then automatically set proper ip address on poolspa securedsowing configuration
 **/
-//Enter Your property Number (original property number)
-$property_number = "";
 
-// Get cURL resource
 $curl = curl_init();
+$apache_port_number ='XX'; // please enter apache port number instead XX
+$proerty_number='XXXX'; // please enter property number instead XXXX
+$ospi_port_number='XXXX'; //please enter  ospi port  number instead XXXX
+
+
+// Set some options - we are passing in a useragent too here
 curl_setopt_array($curl, array(
     CURLOPT_RETURNTRANSFER => 1,
     CURLOPT_URL => 'http://securedshowing.com/dynamic_ipaddress/getip',
     CURLOPT_USERAGENT => 'Codular Sample cURL Request',
     CURLOPT_POST => 1,
     CURLOPT_POSTFIELDS => array(
-        'pro_number' => $property_number,
+        'pro_number' => $proerty_number,
+	  'apache_port_number' => $apache_port_number,
+	'ospi_port_number' => $ospi_port_number
         
     )
 ));
